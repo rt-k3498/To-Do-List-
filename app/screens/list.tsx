3913e93c3c87edf5359a238dd09e0e5f7cae94e8
Arty <aatharvakawade@gmail.com>
2024-12-12 11:50:14 +0100

    syncing the firebase database and adding a tasks bar and making it scrollable

--- app/screens/list.tsx
+++ app/screens/list.tsx
@@ -1,6 +1,6 @@
-import { addDoc, collection, query, where } from "firebase/firestore";
+import { addDoc, collection,getDocs} from "firebase/firestore";
 import { View,Text,StyleSheet,Button, Touchable, TouchableOpacity} from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 import { db } from "../../firebaseConfig";
 import { useState,useEffect } from "react";
 
@@ -8,13 +8,18 @@ import { useState,useEffect } from "react";
 
 
 export default function List() {
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState<any[]>([])
     const [task, setTask] = useState('')
 
     useEffect(() => {
-        //const data = query(collection(db, 'tasks'), where('done', '==', false))
-        //console.log(data)
-
+        async function fetchData() {
+            const data = await getDocs(collection(db, 'tasks'))
+            data.docs.map( (doc) => {
+              setTasks((prev) => [...prev,doc.data()])
+              console.log(doc.data())
+            })
+        }
+        fetchData()
     },[])
 
     function handleChange (text:any) {
@@ -23,16 +28,33 @@ export default function List() {
 
     function addTask () {
         addDoc(collection(db, 'tasks'), {Note: task, done: false})
+        setTasks((prev) => [{Note: task, done: false},...prev])
+        setTask('')
     }
 
   return (
     <View style={styles.container}>
+      <ScrollView style={{flexGrow:1,width:'100%',}}>
         <View style={styles.inputSection}>
-            <TextInput style={styles.input} placeholder='Add a task' onChangeText={(text) => handleChange(text)}/>
+            <TextInput style={styles.input} value={task} placeholder='Add a task' onChangeText={(text) => handleChange(text)}/>
             <TouchableOpacity style={styles.button} onPress={() => addTask()}>
                 <Text style={{color:'white',fontSize:20,}} >Add</Text>
             </TouchableOpacity>
         </View>
+        <View style={styles.taskDisplay} >
+            {tasks.map((items,index)=>(
+              <View key={index} style={styles.taskHolder}>
+                <View style={styles.tasks} >
+                  <Text style={{flexWrap: 'wrap',}}>{items.Note}</Text>
+                </View>
+                <TouchableOpacity style={styles.checkButton}>
+                  <Text>Done</Text>
+                </TouchableOpacity>
+              </View>
+
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -54,8 +76,6 @@ const styles = StyleSheet.create({
         width: '20%',
         height: 40,
         margin: 10,
-        borderColor: 'black',
-        borderWidth: 1,
         borderRadius: 10,
         alignItems: 'center',
         justifyContent: 'center',
@@ -64,13 +84,41 @@ const styles = StyleSheet.create({
   },
     input: {
         flex: 1,
-        width: '80%',
         height: 60,
-        borderColor: 'gray',
+        borderColor: 'black',
         borderWidth: 1,
         borderRadius: 10,
-        margin: 20,
+        margin: 15,
         padding: 10,
+    },
+    taskDisplay:{
+      flex: 1,
+      width: '100%',
+      flexDirection: 'column',
+      padding: 0,
+    },
+    taskHolder:{
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      margin: 10,
+    },
+    tasks: {
+      flex: 5,
+      borderWidth: 1,
+      padding: 8, 
+      margin: 10,
+      borderRadius: 15,
+
+    },
+    checkButton: {
+      flex: 1,
+      borderWidth: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 2,
+
     },
 });
 
